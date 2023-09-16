de6254ca19114fe26b6b89fc32f7e7ad778fbebf
Arijit Nandy <nandyarijit1610@gmail.com>
2023-09-16 23:43:51 +0530

    database added

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 from flask import Flask, render_template, request
 import mysql.connector
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 import json
 from flask_mail import Mail, Message
+import datetime
 with open('config.json', 'r') as c:
     params = json.load(c)["params"]
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codding thrunder'
-# db = SQLAlchemy(app)
-myconn = mysql.connector.connect(host = "localhost", user = "root",passwd = "arijit",database="coddingthunders")  
-cur = myconn.cursor()  
-
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'password'
-# app.config['MYSQL_DB'] = 'coddingthunders'
-# class contacts(db.Model):
-#     sno = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     phone_number = db.Column(db.String(12), nullable=False)
-#     msg = db.Column(db.String(120), nullable=False)
-#     date = db.Column(db.String(12), nullable=False)
-#     email = db.Column(db.String(20), nullable=False)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:arijit@localhost/coddingthunders'
+db = SQLAlchemy(app)
+class contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(12), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(21), nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    img_file = db.Column(db.String(45), nullable=True)
 app.config.update(
     MAIL_SERVER = 'smtp.gmail.com',
     MAIL_PORT = '465',
@@ -51,35 +52,25 @@ def about():
 def contact():
 	if request.method =='POST':
 		# '''d mm'''
-		name = request.form["name"]
-		phone = request.form["num"]
-		message = request.form["message"]
-		date = request.form["date"]
-		email = request.form["email"]
-		# entry = contacts(name=name, phone_number = phone,date = date ,msg = message,email = email )
-		# db.session.add(entry)
-		# db.session.commit()
-		# cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cur.execute("INSERT INTO contacts (name,phone,message,email) VALUES (%s,%s,%s,%s)",(name,phone,message,email))
-		myconn.commit()
-	# 	mail.send_message('New message from blog' + name,
-    # sender = email,
-    # recipients = [params['gmail-user']],
-    # body = message + "\n" +phone ,)
-		msg = Message('Hello', sender = params['gmail-user'], recipients = ['nandyarijit1610@gmail.com',params['gmail-user']]) 
-		msg.body = "Hello Flask message sent from Flask-Mail" + f'\n{name} This mail is sent using flask app' + f'\n{phone}' + f'\n{email}'
-		mail.send(msg)
+		if(request.method=='POST'):
+			'''Add entry to the database'''
+			name = request.form.get('name')
+			email = request.form.get('email')
+			phone = request.form.get('num')
+			date = request.form.get('date')
+			message = request.form.get('message')
+			entry = contacts(name=name, phone = phone, message = message, date= date,email = email )
+			db.session.add(entry)
+			db.session.commit()
+			msg = Message('Hello', sender = params['gmail-user'], recipients = ['nandyarijit1610@gmail.com',params['gmail-user']]) 
+			msg.body = "Hello Flask message sent from Flask-Mail" + f'\n{name} This mail is sent using flask app' + f'\n{phone}' + f'\n{email}'
+			mail.send(msg)
 	return render_template('contact.html',params=params)
 
-
-@app.route("/blog")
-def blog_page():
-	return render_template('blog.html',params=params)
-@app.route("/")
-@app.route("/home", methods=['GET'])
-def post():
-	cur.execute("select * from post where slug = 'first post'")
-	result = cur.fetchall()
-	return render_template('index.html',params=params, result = result)
+#DELETE t1 FROM contacts t1  INNERJOIN contacts t2 WHERE t1.id < t2.id AND t1.email = t2.email;
+ 
+@app.route("/blog/<post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('index.html', params=params, post=post)
 app.run(debug=True)
-
